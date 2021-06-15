5eb928bf14413f232519e792881f3da7563ee860
NickGolitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2021-06-15 22:45:46 +0300

    v2

--- priorglass/src/pages/admin.js
+++ priorglass/src/pages/admin.js
@@ -1,9 +1,13 @@
+import Button from 'react-bootstrap/Button'
 
 function Admin() {
   return (
     <div>
       <main>
-        admin page
+        <div className="d-flex justify-content-center my-3">
+          <Button className="mx-3">Добавить товар</Button>
+          <Button className="mx-3">Добавить тип</Button>
+        </div>
       </main>
     </div>
   );
